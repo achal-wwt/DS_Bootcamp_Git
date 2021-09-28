514ba2779034c1e42f41150e39818d492f868249
Sharma <sharmaac@wwt.com>
2021-09-28 16:36:50 +0530

    Added implementation by Achal

--- src/calculator.py
+++ src/calculator.py
@@ -12,6 +12,12 @@ class Calculator:
     def multiply(self):
         return self.a * self.b
 
+    def divide_achal(self):
+        # TODO:- Add your code in the below function
+        # check for divide by zero error, using assert statements
+        # implement the division operation
+        return
+
     def divide_PUT_YOUR_NAME_HERE(self):
         # TODO:- Add your code in the below function
         # check for divide by zero error, using assert statements
